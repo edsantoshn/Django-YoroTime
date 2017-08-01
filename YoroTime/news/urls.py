3c7cdee8f69181c15491af62b5617024f4dbd559
Edu Santos Paz <eduardo@edsantoshn.com>
2017-07-31 21:10:50 -0600

    URL

--- YoroTime/news/urls.py
+++ YoroTime/news/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^/$', views.AuthorsIndex.as_view()),
-    url(r'^/$', views.PostsIndex.as_view()),
-    url(r'^/$', views.PostDetail.as_view()),
-    url(r'^/$', views.AuthorPosts.as_view()),
-    url(r'^/$', views.CommentsPost.as_view()),
+    url(r'^authors/$', views.AuthorsIndex.as_view()),
+    url(r'^posts/$', views.PostsIndex.as_view()),
+    url(r'^posts/(?P<pk>\d+)/$', views.PostDetail.as_view()),
+    url(r'^posts/author/(?P<fk>\d+)/$', views.AuthorPosts.as_view()),
+    url(r'^posts/comment/(?P<fk>\d+)$', views.CommentsPost.as_view()),
+    url(r'^posts/author/time/$', views.AuthorPostsSince.as_view()),
+    url(r'^posts/author/time/$', views.AuthorPostsAfter.as_view()),
 ]
